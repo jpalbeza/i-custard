b154c74740b8778f2a1095d8ea7e0b16cd7cc7ec
John Paul Albeza <johnpaul.albeza@gmail.com>
2019-04-14 23:56:27 +1000

    Add free-vga promo. Refactor apply-promo to direct sub-line-item

--- cart/promos.py
+++ cart/promos.py
@@ -13,11 +13,6 @@ def tag_every_3rd_atv(cart_items):
                 apple_tv_count = 0
 
 
-def free_atv(item):
-    item["sub-items"].append(copy.deepcopy({"label": "3 Apple TVs for 2",
-                                            "price": (-1 * catalogue.products["atv"]["price"])}))
-
-
 def tag_bulk_ipd(cart_items):
     ipad_count = 0
     for item in cart_items:
@@ -32,16 +27,27 @@ def tag_bulk_ipd(cart_items):
                 item["promos"].append("ipd-bulk-4")
 
 
-def discounted_ipd(item):
-    target_price = 499.99
-    item["sub-items"].append(copy.deepcopy({"label": "4 iPads Bulk Discount",
-                                            "price": (-1 * (catalogue.products["ipd"]["price"] - target_price))}))
+def tag_all_macbook_pro(cart_items):
+    for item in cart_items:
+        if item["sku"] == "mbp":
+            item["promos"].append("free-vga-for-macbook-pro")
+
+
+def free_vga():
+    vga = copy.deepcopy(catalogue.products["vga"])
+    vga["price"] = 0.0
+
+    return vga
 
 
 promos = {"atv-3-for-2": {"tag-applicable": tag_every_3rd_atv,
-                          "apply-promo": free_atv},
+                          "promo-item": {"label": "3 Apple TVs for 2",
+                                         "price": (-1 * catalogue.products["atv"]["price"])}},
           "ipd-bulk-4": {"tag-applicable": tag_bulk_ipd,
-                         "apply-promo": discounted_ipd}}
+                         "promo-item": {"label": "4+ iPads Bulk Discount",
+                                        "price": (-1 * (catalogue.products["ipd"]["price"] - 499.99))}},
+          "free-vga-for-macbook-pro": {"tag-applicable": tag_all_macbook_pro,
+                                       "promo-item": free_vga()}}
 
 
 def tag_with_promos(cart_items):
@@ -52,4 +58,5 @@ def tag_with_promos(cart_items):
 def apply_promos(cart_items):
     for item in cart_items:
         for promo in item["promos"]:
-            promos[promo]["apply-promo"](item)
+            promo_item = copy.deepcopy(promos[promo]["promo-item"])
+            item["sub-items"].append(promo_item)
